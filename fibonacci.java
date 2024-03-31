3d1e9e969f62a874d5ca462891c0dd7dc6000bf2
kruthikha <01fe22bca164@kletech.ac.in>
2024-03-31 21:03:45 +0530

    fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = s.nextInt();
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
